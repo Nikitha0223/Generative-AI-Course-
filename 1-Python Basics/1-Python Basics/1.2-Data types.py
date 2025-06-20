b4204f61b173fd9adc140ada61061132d4c8dc96
Nikitha0223 <nikitha.thimmanaboyena26@gmail.com>
2025-06-20 08:50:07 +0530

    Create 1.2-Data types.py

---
+++ 1-Python Basics/1-Python Basics/1.2-Data types.py
@@ -0,0 +1,56 @@
+1. Integer and Float
+a = 10
+b = 3.14
+print(type(a))
+print(type(b))
+Output:
+<class 'int'>
+<class 'float'>
+
+2. String
+name = "Python"
+print(type(name))
+print("Welcome to", name)
+Output:
+<class 'str'>
+Welcome to Python
+
+3. Boolean
+is_ai_fun = True
+print(type(is_ai_fun))
+print("Is AI fun?", is_ai_fun)
+Output:
+<class 'bool'>
+Is AI fun? True
+
+4. List
+fruits = ["apple", "banana", "cherry"]
+print(type(fruits))
+print("First fruit:", fruits[0])
+Output:
+<class 'list'>
+First fruit: apple
+
+5. Tuple
+coordinates = (10, 20)
+print(type(coordinates))
+print("X value:", coordinates[0])
+Output:
+<class 'tuple'>
+X value: 10
+
+6. Dictionary
+student = {"name": "Alice", "age": 21}
+print(type(student))
+print("Student Name:", student["name"])
+Output:
+<class 'dict'>
+Student Name: Alice
+
+7. Set
+unique_nums = {1, 2, 3, 3, 2}
+print(type(unique_nums))
+print(unique_nums)
+Output:
+<class 'set'>
+{1, 2, 3}
